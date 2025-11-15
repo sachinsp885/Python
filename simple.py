d9080661b4542cec591ee67bade34289675b7c2d
sachinsp885 <sachinpattanashetti99@gmail.com>
2025-11-16 00:18:27 +0530

    simple

---
+++ simple.py
@@ -0,0 +1,12 @@
+# Simple Python Program
+
+# Print a message
+print("Hello, World!")
+
+# Add two numbers
+a = 5
+b = 3
+sum = a + b
+
+# Display the result
+print("The sum of", a, "and", b, "is", sum)
